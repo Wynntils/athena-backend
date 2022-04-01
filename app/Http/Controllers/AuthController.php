a12d2c6c210eb68364fd90fffe5ac2ae10b949da
Scyu_ <scyu@scyu.dev>
2022-04-01 07:01:18 +0100

    revert

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -25,12 +25,7 @@ class AuthController extends Controller
             Storage::put('request.json', $request->getContent());
         }
 
-        // $profile = MinecraftFakeAuth::instance()->getGameProfile($request->validated('username'), $request->validated('key'));
-
-        $profile = [
-            'id' => 'af4addfc-c030-4fe5-a2d6-1061e8c96386',
-            'name' => 'Syaoran3'
-        ];
+        $profile = MinecraftFakeAuth::instance()->getGameProfile($request->validated('username'), $request->validated('key'));
 
         if ($profile === null) {
             return response()->json(['message' => 'The provided username or key is invalid'], 401);
