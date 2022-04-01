92051b5fd0ca26e346a1fb0dc622b7265bec36fb
Scyu_ <scyu@scyu.dev>
2022-04-01 04:04:13 +0100

    Bugfixing

--- app/Http/Controllers/CacheController.php
+++ app/Http/Controllers/CacheController.php
@@ -8,7 +8,7 @@ class CacheController extends Controller
 {
     public function getCache($cacheName): \Illuminate\Http\JsonResponse
     {
-        return response()->json(CacheManager::getCache($cacheName)) ?? response()->json(['message' => "There's not a cache with the provided name."], 404);
+        return response()->json(CacheManager::getCache($cacheName), headers: ['timestamp' => currentTimeMillis()]) ?? response()->json(['message' => "There's not a cache with the provided name."], 404);
     }
 
     public function getHashes(): \Illuminate\Http\JsonResponse
