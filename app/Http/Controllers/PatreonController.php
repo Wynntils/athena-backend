08ccb584bde72985dd22fdd88c38a2cd08791322
Scyu_ <scyu@scyu.dev>
2022-04-03 13:51:06 +0100

    Fix import

--- app/Http/Controllers/PatreonController.php
+++ app/Http/Controllers/PatreonController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use DiscordWebhook\Embed;
 use DiscordWebhook\EmbedColor;
 use DiscordWebhook\Webhook;
-use Illuminate\Support\Facades\Request;
+use Illuminate\Http\Request;
 
 class PatreonController extends Controller
 {
