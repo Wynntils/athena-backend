646bbd017f7498aeb0be64de246c02797d0c3c6b
Scyu_ <scyu@scyu.dev>
2022-04-04 00:03:34 +0100

    Refactor patreon webhook

--- app/Http/Controllers/PatreonController.php
+++ app/Http/Controllers/PatreonController.php
@@ -6,6 +6,8 @@ use DiscordWebhook\Embed;
 use DiscordWebhook\EmbedColor;
 use DiscordWebhook\Webhook;
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
+use Storage;
 
 class PatreonController extends Controller
 {
@@ -19,42 +21,9 @@ class PatreonController extends Controller
             abort(403, 'Invalid signature');
         }
 
-        $eventData = json_decode($payload, true);
+        $data = collect(json_decode($payload, true));
 
-        $pledgeAmount = $eventData['data']['attributes']['will_pay_amount_cents'];
-        $pledgeStatus = $eventData['data']['attributes']['last_charge_status'];
-        $patronId = $eventData['data']['relationships']['user']['data']['id'];
-        $campaignId = $eventData['data']['relationships']['campaign']['data']['id'];
-        foreach ($eventData['included'] as $included_data) {
-            if ($included_data['type'] === 'user' && $included_data['id'] === $patronId) {
-                $userData = $included_data;
-            }
-            if ($included_data['type'] === 'campaign' && $included_data['id'] === $campaignId) {
-                $campaignData = $included_data;
-            }
-        }
-
-        $patronUrl = $userData['attributes']['url'];
-        $patronImage = $userData['attributes']['image_url'];
-        $patronFullName = $userData['attributes']['full_name'];
-        $patronCount = $campaignData['attributes']['patron_count'];
-        $discord = $userData['attributes']['social_connections']['discord'];
-
-        if ($discord !== null) {
-            $discord = $discord['user_id'];
-        }
-
-        $this->sendDiscordMessage(
-            $request->header('X_PATREON_EVENT'),
-            $discord !== null ? " <@{$discord}>" : null,
-            $patronFullName,
-            $patronUrl,
-            $patronImage,
-            $pledgeAmount,
-            $pledgeStatus,
-            $patronCount
-        );
-
+        return $this->processEvent($data);
     }
 
     private function verifySignature($payload, $signature)
@@ -64,10 +33,57 @@ class PatreonController extends Controller
         return hash_equals(hash_hmac('sha256', $payload, $secret), $signature);
     }
 
+    private function processEvent(Collection $data): bool
+    {
+        $includedData = collect($data->get('included'));
+        $eventData = collect($data->get('data'));
+
+        $attributes = collect($eventData->get('attributes'));
+        $pledgeAmount = $attributes->get('will_pay_amount_cents');
+        $pledgeStatus = $attributes->get('last_charge_status');
+        $pledgeMonths = $attributes->get('pledge_cadence');
+
+        $relationships = collect($eventData->get('relationships'));
+        $patronId = $relationships->pull('user.data.id');
+        $campaignId = $relationships->pull('campaign.data.id');
+        $tierId = $relationships->pull('currently_entitled_tiers.data.0.id');
+
+        $userData = collect($includedData->where('type', 'user')->where('id', $patronId)->first());
+        $campaignData = collect($includedData->where('type', 'campaign')->where('id', $campaignId)->first());
+        $tierData = collect($includedData->where('type', 'tier')->where('id', $tierId)->first());
+
+        $userAttributes = collect($userData->get('attributes'));
+        $campaignAttributes = collect($campaignData->get('attributes'));
+        $tierAttributes = collect($tierData->get('attributes'));
+
+        $tier = $tierAttributes->get('title');
+
+        $patronUrl = $userAttributes->get('url');
+        $patronImage = $userAttributes->get('image_url');
+        $patronFullName = $userAttributes->get('full_name');
+        $discord = $userAttributes->get('social_connections.discord.user_id');
+        $patronCount = $campaignAttributes->get('patron_count');
+
+        return $this->sendDiscordMessage(
+            \Request::header('X_PATREON_EVENT'),
+            $discord !== null ? " <@{$discord}>" : null,
+            $patronFullName,
+            $tier,
+            $pledgeMonths,
+            $patronUrl,
+            $patronImage,
+            $pledgeAmount,
+            $pledgeStatus,
+            $patronCount
+        );
+    }
+
     private function sendDiscordMessage(
         $eventType,
         $message,
         $fullName,
+        $tier,
+        $pledgeMonths,
         $patronUrl,
         $patronImage,
         $pledgeAmount,
@@ -81,9 +97,12 @@ class PatreonController extends Controller
             default => EmbedColor::RED
         };
 
-        $webhook = new Webhook(config('services.patreon.discord_webhook'));
+        $wh = new Webhook(config('services.patreon.discord_webhook'));
+        $wh->setUsername('Patreon')
+            ->setAvatar('https://c5.patreon.com/external/favicon/apple-touch-icon.png')
+            ->setMessage($message);
+
         $embed = new Embed();
-        $webhook->setMessage($message);
         $embed->setColor($color)->setAuthor(
             (new Embed\Author())
                 ->setName($fullName)
@@ -92,17 +111,33 @@ class PatreonController extends Controller
         )->addField(
             (new Embed\Field())
                 ->setName('Amount')
-                ->setValue($pledgeAmount)
+                ->setValue(number_format(($pledgeAmount / 100), 2, '.', ' '))
                 ->setIsInline(true)
         )->addField(
             (new Embed\Field())
                 ->setName('Payment Status')
                 ->setValue($pledgeStatus)
                 ->setIsInline(true)
+        )->addField(
+            (new Embed\Field())
+                ->setName('Months')
+                ->setValue($pledgeMonths)
+                ->setIsInline(true)
+        )->addField(
+            (new Embed\Field())
+                ->setName('Tier')
+                ->setValue($tier)
+                ->setIsInline(false)
         )->setFooter((new Embed\Footer())
-            ->setText($eventType." | Total Patrons: ".$patronCount)
+            ->setText($eventType." | Total Patrons: ".$patronCount." | TEST ")
         );
-        $webhook->addEmbed($embed)->send();
+        return $wh->addEmbed($embed)->send();
+    }
+
+    public function test(Request $request)
+    {
+        $data = collect(json_decode(Storage::get('test.json'), true));
+        $this->processEvent($data);
     }
 
 }
--- routes/patreon.php
+++ routes/patreon.php
@@ -4,4 +4,5 @@ use App\Http\Controllers\PatreonController;
 
 Route::controller(PatreonController::class)->group(static function () {
     Route::post('webhook', 'webhook');
+    Route::get('test', 'test')->middleware('auth:api');
 });
