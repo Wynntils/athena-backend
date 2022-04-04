855054ca28a4cdad4e39648480e3d604ac275faf
Scyu_ <scyu@scyu.dev>
2022-04-04 21:01:09 +0100

    Update analytics handling

--- app/Http/Middleware/Analytics.php
+++ app/Http/Middleware/Analytics.php
@@ -19,20 +19,18 @@ class Analytics
      */
     public function handle(Request $request, Closure $next)
     {
-        async(function () use ($request) {
-            $clientId = $this->getClientId($request);
-            /** @var GoogleAnalytics $gamp */
-            $gamp = GAMP::setClientId($clientId);
-            $gamp->setDocumentPath('/'.$request->path());
-            $gamp->setDocumentReferrer($request->server('HTTP_REFERER', ''));
-            $gamp->setUserAgentOverride($request->server('HTTP_USER_AGENT', 'Missing/1.0'));
+        $clientId = $this->getClientId($request);
+        /** @var GoogleAnalytics $gamp */
+        $gamp = GAMP::setClientId($clientId);
+        $gamp->setDocumentPath('/'.$request->path());
+        $gamp->setDocumentReferrer($request->server('HTTP_REFERER', ''));
+        $gamp->setUserAgentOverride($request->server('HTTP_USER_AGENT', 'Missing/1.0'));
 
-            // Override the sent IP with the IP from the current request.
-            // Otherwhise the servers IP would be sent.
-            $gamp->setIpOverride($request->ip());
+        // Override the sent IP with the IP from the current request.
+        // Otherwhise the servers IP would be sent.
+        $gamp->setIpOverride($request->ip());
 
-            $gamp->sendPageview();
-        });
+        $gamp->sendPageview();
 
         return $next($request);
     }
