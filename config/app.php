35fb14ddbe612a8aca0a0b5b9cbc7ee31586a6f5
Scyu_ <scyu@scyu.dev>
2022-04-10 14:31:54 +0100

    Fix bug

--- config/app.php
+++ config/app.php
@@ -146,7 +146,7 @@ return [
 //        Illuminate\Bus\BusServiceProvider::class,
         Illuminate\Cache\CacheServiceProvider::class,
         Illuminate\Foundation\Providers\ConsoleSupportServiceProvider::class,
-//        Illuminate\Cookie\CookieServiceProvider::class,
+        Illuminate\Cookie\CookieServiceProvider::class,
         Illuminate\Database\DatabaseServiceProvider::class,
 //        Illuminate\Encryption\EncryptionServiceProvider::class,
         Illuminate\Filesystem\FilesystemServiceProvider::class,
