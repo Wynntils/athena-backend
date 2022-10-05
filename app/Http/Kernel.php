3971b9ebf968c9b7fd40f2da55737e5b9619152c
Scyu_ <scyu@scyu.dev>
2022-10-06 00:39:48 +0100

    chore: disable monitor middleware

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -23,7 +23,7 @@ class Kernel extends HttpKernel
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
-        \App\Http\Middleware\Monitor::class,
+//        \App\Http\Middleware\Monitor::class,
         \App\Http\Middleware\CacheControl::class,
     ];
 
