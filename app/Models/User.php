1a166a453174473ec2ed135e28fce94938c0a074
Scyu_ <scyu@scyu.dev>
2022-10-05 14:14:21 +0100

    chore: add user seeder

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,6 +10,7 @@ use Illuminate\Auth\Authenticatable;
 use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\Access\Authorizable;
 use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
@@ -28,7 +29,7 @@ class User extends Model implements
     AuthenticatableContract,
     AuthorizableContract
 {
-    use Authenticatable, Authorizable;
+    use Authenticatable, Authorizable, HasFactory;
 
     public $timestamps = false;
     /**
---
+++ database/factories/UserFactory.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Http\Enums\AccountType;
+use App\Models\Casts\PartInfo;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Str;
+
+class UserFactory extends Factory
+{
+    protected $model = User::class;
+
+    public function definition()
+    {
+        return [
+            'username' => $this->faker->userName(),
+            'authToken' => Str::random(10),
+            'accountType' => $this->faker->randomElement(AccountType::cases()),
+            'discordInfo' => [
+                'username' => $this->faker->userName(),
+                'id' => $this->faker->randomNumber()
+            ],
+            'cosmeticInfo' => [
+                'capeTexture' => $this->faker->randomElement(['cape1', 'cape2', 'cape3']),
+                'elytraEnabled' => $this->faker->boolean(),
+                'maxResolution' => $this->faker->randomElement(['64x32', '128x64', '256x128', '512x256']),
+                'allowAnimated' => $this->faker->boolean(),
+                'parts' => [
+                    'ears' => $this->faker->boolean()
+                ]
+            ]
+        ];
+    }
+}
