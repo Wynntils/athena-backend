7386d082503d28e955899612e7fd4bbbfecf08c4
Scyu_ <scyu@scyu.dev>
2022-10-10 13:24:11 +0100

    chore: add md5 hash with cache

--- app/Http/Controllers/VersionController.php
+++ app/Http/Controllers/VersionController.php
@@ -14,8 +14,9 @@ class VersionController extends Controller
     public function latest(Request $request, $stream)
     {
         $isArtemis = str($request->userAgent())->contains('Artemis');
+        $client = $isArtemis ? 'Artemis' : 'Wynntils';
 
-        $releases = $this->getReleases($isArtemis ? 'Artemis' : 'Wynntils', $stream);
+        $releases = $this->getReleases($client, $stream);
 
         $latest = $releases->first();
 
@@ -23,10 +24,20 @@ class VersionController extends Controller
             return response()->json(['error' => 'No release found for this stream'], 404);
         }
 
+        $cache = \Cache::get('version', []);
+        if (!isset($cache[$stream][$client])) {
+            $cache[$stream][$client] = [];
+            $cache = $this->updateCache($cache, $stream, $client, $latest);
+        }
+
+        if ($cache[$stream][$client]['tag'] !== $latest['tag_name']) {
+            $cache = $this->updateCache($cache, $stream, $client, $latest);
+        }
+
         return response()->json([
             'version' => $latest['tag_name'],
             'url' => $latest['assets'][0]['browser_download_url'],
-            'commit' => $latest['target_commitish'],
+            'md5' => $cache[$stream][$client]['md5'],
             'changelog' => $latest['body'],
         ]);
     }
@@ -36,5 +47,17 @@ class VersionController extends Controller
         return collect($this->github->repo()->releases()->all('Wynntils', $repo))->filter(function ($release) use ($stream) {
             return $release['prerelease'] === ($stream === 'ce');
         });
+    }
+
+    private function updateCache(mixed $cache, $stream, string $client, mixed $latest)
+    {
+        $cache[$stream][$client] = [
+            'tag' => $latest['tag_name'],
+            'md5' => md5_file($latest['assets'][0]['browser_download_url']),
+        ];
+
+        \Cache::put('version', $cache);
+
+        return $cache;
     }
 }
