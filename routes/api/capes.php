98838230e936fae988be11bfb39fe6ad54697c1f
Scyu_ <scyu@scyu.dev>
2023-03-27 09:07:54 +0100

    fix: cape routes

--- routes/api/capes.php
+++ routes/api/capes.php
@@ -11,6 +11,6 @@ Route::controller(CapeController::class)->group(static function () {
         Route::get('get/{sha}', 'queueGetCape')->name('get');
         Route::get('list', 'queueList')->name('list');
         Route::get('approve/{token}/{sha}/{type}', 'approveCape')->middleware('cape.token')->name('approve');
-        Route::post('upload/{token}', 'uploadCape')->middleware('token');
+        Route::post('upload/{token}', 'uploadCape')->middleware('cape.token');
     });
 });
