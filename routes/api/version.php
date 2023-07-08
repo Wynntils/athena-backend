b31a1e21726332c5fa6289a14f3bfd8b3f981e56
Scyu_ <scyu@scyu.dev>
2023-07-08 12:41:10 +0100

    fix: download stream

--- routes/api/version.php
+++ routes/api/version.php
@@ -4,5 +4,5 @@ use App\Http\Controllers\VersionController;
 
 Route::get('latest/{stream}', [VersionController::class, 'latest'])->name('latest');
 Route::get('changelog/{version}', [VersionController::class, 'changelog'])->name('changelog');
-Route::get('download/{version}/{stream}/{modloader?}', [VersionController::class, 'download'])->where('stream', 're|ce')->name('download');
+Route::get('download/{version}/{stream}/{modloader?}', [VersionController::class, 'download'])->name('download');
 Route::get('changelog/{version1}/{version2}', [VersionController::class, 'changelogBetween'])->name('changelogBetween');
