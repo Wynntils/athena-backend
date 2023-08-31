5f1eff69076cb31267e6f753906db17bc29796f8
Lucas <chromium@wynntils.com>
2023-08-31 17:38:24 +0200

    refactor: add filter to guild list route (#31)
    * refactor: add filter to guild list route
    
    * refactor: remove unnecessary else statement

--- routes/api/guilds.php
+++ routes/api/guilds.php
@@ -3,6 +3,6 @@
 use \App\Http\Controllers\GuildController;
 
 Route::controller(GuildController::class)->group(static function() {
-   Route::get('list', 'list')->name('list');
+   Route::get('list/{filter?}', 'list')->name('list');
    Route::post('setColor/{apiKey}', 'setColor')->middleware(['athena.token'])->name('setColor');
 });
