d421e51eeeae08e328629d1a2c9f0d45b01cfa7f
Lucas <chromium@wynntils.com>
2023-08-31 18:17:47 +0200

    feat: make guild list a cache (#32)

---
+++ app/Http/Libraries/Requests/Cache/GuildList.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Libraries\Requests\Cache;
+
+use App\Models\Guild;
+
+class GuildList implements CacheContract
+{
+
+    public function refreshRate(): int
+    {
+        return 3600;
+    }
+
+    public function generate(): array
+    {
+        return Guild::all()->toArray();
+    }
+}
--- routes/api/guilds.php
+++ routes/api/guilds.php
@@ -3,6 +3,5 @@
 use \App\Http\Controllers\GuildController;
 
 Route::controller(GuildController::class)->group(static function() {
-   Route::get('list/{filter?}', 'list')->name('list');
    Route::post('setColor/{apiKey}', 'setColor')->middleware(['athena.token'])->name('setColor');
 });
