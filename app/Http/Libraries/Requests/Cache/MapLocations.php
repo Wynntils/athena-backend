045b858d5f69e9e0c043a0f6d1bbd4e12c4a7077
Scyu_ <scyu@scyu.dev>
2023-10-18 12:39:06 +0100

    chore: MapLocations v3 migration

--- app/Http/Libraries/Requests/Cache/MapLocations.php
+++ app/Http/Libraries/Requests/Cache/MapLocations.php
@@ -16,36 +16,37 @@ class MapLocations implements CacheContract
 
     public function generate(): array
     {
+        /** @var \Illuminate\Http\Client\Response[] $responses */
         $responses = Http::wynn()->pool(fn (Pool $pool) => [
-            $pool->as('wynnMapLocations')->get(config('athena.api.wynn.mapLocations')),
+            $pool->as('wynnMapLocations')->get(config('athena.api.wynn.v3.mapLocations')),
             $pool->as('wynnMapLabels')->get(config('athena.api.wynn.mapLabels')),
             $pool->as('npcLocations')->get(config('athena.api.wynn.npcLocations')),
         ]);
 
+        $response = [];
 
-        $wynnMapLocations = $responses['wynnMapLocations']->collect();
+        $wynnMapLocations = $responses['wynnMapLocations']->json();
         if ($wynnMapLocations === null) {
             throw new \Exception('Failed to fetch map locations from Wynn API');
         }
-        $wynnMapLocations->forget('request');
-        $wynnMapLocations['locations'] = collect($wynnMapLocations['locations'])->filter(function ($location) {
+        $response['locations'] = collect($wynnMapLocations)->filter(function ($location) {
             return $location['icon'] !== "Content_Raid.png" && $location['icon'] !== "Special_SeaskipperFastTravel.png" && $location['icon'] !== "Special_HousingAirBalloon.png";
-        })->values()->toArray();
+        })->values();
 
         $wynnMapLabels = $responses['wynnMapLabels']->collect();
         if ($wynnMapLabels === null) {
             throw new \Exception('Failed to fetch map labels from Data Storage');
         }
 
-        $wynnMapLocations['labels'] = $wynnMapLabels['labels'];
+        $response['labels'] = $wynnMapLabels['labels'];
 
         $npcLocations = $responses['npcLocations']->collect();
         if ($npcLocations === null) {
             throw new \Exception('Failed to fetch NPC locations from Data Storage');
         }
 
-        $wynnMapLocations['npc-locations'] = $npcLocations['npc-locations'];
+        $response['npc-locations'] = $npcLocations['npc-locations'];
 
-        return $wynnMapLocations->toArray();
+        return $response;
     }
 }
