e3e60d491970cea256bd9c52a7adb6fded753a1a
Lucas <chromium@wynntils.com>
2024-07-06 22:33:03 +0200

    FEAT: middleware to block user agents (#36)

--- routes/api/v2/user.php
+++ routes/api/v2/user.php
@@ -3,5 +3,7 @@
 use App\Http\Controllers\UserController;
 
 Route::controller(UserController::class)->group(static function () {
-    Route::post('getInfo', 'getInfoV2')->name('getInfoV2');
+    Route::post('getInfo', 'getInfoV2')
+        ->middleware('block.user.agents')
+        ->name('getInfoV2');
 });
