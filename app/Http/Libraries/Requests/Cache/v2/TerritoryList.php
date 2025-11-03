c90ea3c4e72aca4675e00b7b5e3f5ae0855bd4ec
Lucas <chromium@wynntils.com>
2025-11-03 20:15:45 +0100

    chore: update territoryList cache to refresh every 15 seconds (#49)
    * chore: update territoryList cache to refresh every 15 seconds + cache the guilds
    
    * fix: sonarqube

--- app/Http/Libraries/Requests/Cache/v2/TerritoryList.php
+++ app/Http/Libraries/Requests/Cache/v2/TerritoryList.php
@@ -6,28 +6,39 @@ namespace App\Http\Libraries\Requests\Cache\v2;
 use App\Http\Libraries\Requests\Cache\CacheContract;
 use App\Models\Guild;
 use Http;
+use Illuminate\Support\Facades\Cache;
 
 class TerritoryList implements CacheContract
 {
 
     public function refreshRate(): int
     {
-        return 30;
+        return 15;
     }
 
     public function generate(): array
     {
-        $wynnTerritories = Http::wynn()->get(config('athena.api.wynn.v3.territories'))->collect();
+        $wynnTerritories = Http::wynn()
+            ->get(config('athena.api.wynn.v3.territories'))
+            ->collect();
+
         if ($wynnTerritories === null) {
             throw new \Exception('Failed to fetch territories from Wynn API');
         }
 
         return $wynnTerritories->map(function ($t) {
-            if (!empty($t['guild']) && !empty($t['guild']['name'])) {
-                $guild = Guild::gather(['guild' => $t['guild']]);
-                $t['guild']['color'] = empty($guild->color)
-                    ? generateColorAndUpdate($guild)
-                    : $guild->color;
+            $prefix = data_get($t, 'guild.prefix');
+            $name   = data_get($t, 'guild.name');
+            if ($name || $prefix) {
+                $id  = $prefix ? 'p:' . mb_strtoupper($prefix) : 'n:' . mb_strtolower($name);
+                $key = 'guild_color:' . hash('sha512', $id);
+
+                $color = Cache::remember($key, 3600, function () use ($t) {
+                    $guild = Guild::gather(['guild' => data_get($t, 'guild')]);
+                    return $guild->color ?: generateColorAndUpdate($guild);
+                });
+
+                data_set($t, 'guild.color', $color);
             }
             return $t;
         })->toArray();
