66f14c3e8fd55de1c2844b13aca9e128b053fd86
DevChromium <chromium@wynntils.com>
2026-02-25 21:23:38 +0100

    refactor: version controller

--- routes/api/version.php
+++ routes/api/version.php
@@ -5,4 +5,3 @@ use App\Http\Controllers\VersionController;
 Route::get('latest/{stream}', [VersionController::class, 'latest'])->name('latest');
 Route::get('changelog/{version}', [VersionController::class, 'changelog'])->name('changelog');
 Route::get('download/{version}/{stream}/{modloader?}', [VersionController::class, 'download'])->name('download');
-Route::get('changelog/{version1}/{version2}', [VersionController::class, 'changelogBetween'])->name('changelogBetween');
