2dd66e10eb3d0f4a1cb0198caed9d90f489cf6cc
DevChromium <chromium@wynntils.com>
2026-02-25 21:32:35 +0100

    chore: no sonar this does not matter

--- tests/scripts/version-stream-check.js
+++ tests/scripts/version-stream-check.js
@@ -82,6 +82,7 @@ export const options = {
 };
 
 export default function () {
+    // NOSONAR
     const c = cases[Math.floor(Math.random() * cases.length)];
     const res = http.get(`${BASE}/version/latest/${c.stream}`, {
         headers: { 'User-Agent': c.ua },
